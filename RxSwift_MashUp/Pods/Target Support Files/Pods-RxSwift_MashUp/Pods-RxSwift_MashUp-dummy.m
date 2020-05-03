98e4a841a100285604007ecfbd228c4634fe6151
rookie.w <wanrage@naver.com>
2020-05-03 19:17:21 +0900

    깃 오류 수정2

---
+++ RxSwift_MashUp/Pods/Target Support Files/Pods-RxSwift_MashUp/Pods-RxSwift_MashUp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RxSwift_MashUp : NSObject
+@end
+@implementation PodsDummy_Pods_RxSwift_MashUp
+@end
---
+++ RxSwift_MashUp/RxSwift_MashUp/SignUpViewModel.swift
@@ -0,0 +1,71 @@
+//
+//  SignUpViewModel.swift
+//  RxSwift_MashUp
+//
+//  Created by rookie.w on 2020/05/03.
+//  Copyright © 2020 rookie.w. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+
+protocol ViewModelType{
+}
+class SignUpViewModel: ViewModelType{
+    var disposeBag = DisposeBag()
+    
+    var confirmedId = PublishSubject<Bool>()
+    var memberDepart = PublishSubject<MemberType>()
+    
+    struct Input{
+        let idText: Observable<String>
+        let depart: Observable<Int>
+    }
+    struct Output{
+        let confirmedId: PublishSubject<Bool>
+        let memberDepart: PublishSubject<MemberType>
+    }
+    func transform(_ input: Input) -> Output{
+        
+        input.idText
+            .map{$0.isValidId()}
+            .bind(to: confirmedId)
+            .disposed(by: disposeBag)
+        
+        input.depart
+            .map{MemberType.init(rawValue: $0)}
+            .compactMap{$0}
+            .bind(to: memberDepart)
+            .disposed(by: disposeBag)
+            
+        
+        
+        
+        return Output(confirmedId: confirmedId, memberDepart: memberDepart)
+    }
+    
+    enum MemberType: Int{
+        case Dev = 0
+        case Design = 1
+        
+        func isDev() -> Bool {
+            return self == .Dev
+        }
+        func toString() -> String {
+            switch self {
+            case .Dev :
+                return "Developer"
+            case .Design :
+                return "Designer"
+            }
+        }
+    }
+}
+
+
+private extension String{
+    func isValidId() -> Bool{
+        return 5...10 ~= self.count
+    }
+}
